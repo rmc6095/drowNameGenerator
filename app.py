f63dfef7b2ed8fac1e1265ce5ce187314d0377e9
Ren <rmc6095@rit.edu>
2024-03-02 18:26:16 -0500

    Prevented the form from clearing chosen options when a new name is generated.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def main():
     name = str(dng.generate_name())
     name = name.split('\n')
-    return render_template("DrowNameGenerator.html", drow_string=name[0], trans_string=name[1])
+    return render_template("DrowNameGenerator.html", drow_string=name[0], trans_string=name[1], last_g="", last_gender='Choose Here', last_l="", last_length='Choose Here', last_o='', last_order='Choose Here', last_vowel='75', last_consonant='50')
 
 
 @app.route('/gen', methods=['GET', 'POST'])
@@ -17,35 +17,52 @@ def generate():
     match gender:
         case "F":
             gender = 0
+            last_g = 'F'
+            last_gender = 'Feminine'
         case "M":
             gender = 1
+            last_g = 'M'
+            last_gender = 'Masculine'
         case "N":
             gender = 2
+            last_g = 'N'
+            last_gender = 'Neutral'
     length = request.form['length']
     match length:
         case "n":
             mid = False
             end = True
+            last_l = 'n'
+            last_length = 'Standard'
         case "l":
             mid = True
             end = True
+            last_l = 'l'
+            last_length = 'Longer'
         case "s":
             mid = False
             end = False
+            last_l = 's'
+            last_length = 'Shorter'
     order = request.form['order']
     match order:
         case "True":
             order = True
+            last_o = 'True'
+            last_order = 'Yes'
         case "False":
             order = False
+            last_o = 'False'
+            last_order = 'No'
         case "None":
             order = None
+            last_o = 'None'
+            last_order = 'Either'
     vowels = int(request.form['vowels'])
     consonants = int(request.form['consonants'])
     name = str(dng.generate_name(gender, mid, end, order, vowels, consonants))
     name = name.split('\n')
-    return render_template("DrowNameGenerator.html", drow_string=name[0], trans_string=name[1])
-
+    return render_template("DrowNameGenerator.html", drow_string=name[0], trans_string=name[1],last_g=last_g, last_gender=last_gender, last_l=last_l, last_length=last_length, last_o=last_o, last_order=last_order, last_vowels=str(vowels), last_consonants=str(consonants))
 
 if __name__ == '__main__':
     app.debug
